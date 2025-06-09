a8603a904a45cc594d544f18483ad0299c3ec965
Reed Ceniviva <44185694+RalexcenTemple@users.noreply.github.com>
2025-06-09 15:23:29 -0400

    code cleanup

--- Scripts/component_systems/entities/entity_worker.gd
+++ Scripts/component_systems/entities/entity_worker.gd
@@ -30,9 +30,11 @@ func setup(_layer_manager: layer_manager):
 	#give the worker sight
 	my_sight_component = ComponentSight.new()
 	my_sight_component.setup(32)
+	set_meta("component_sight", my_sight_component)
 	
 	#give the worker legs and pathing
 	my_movement_component = ComponentMovement.new()
+	set_meta("component_movement", my_movement_component)
 	
 	#give the worker an identity
 	my_char_stats = ComponentCharstats.new()
@@ -41,22 +43,19 @@ func setup(_layer_manager: layer_manager):
 	char_name = my_char_stats.FIRST_NAMES.pick_random()
 	char_sex = randi()%2
 	my_char_stats.setup(char_name,char_sex, 0.0, 0.25, 0.25)
+	set_meta("component_charstats", my_char_stats)
 	
 	#give the worker processing
 	my_tasks = ComponentTasks.new()
+	set_meta("component_tasks", my_tasks)
 	
 	#let the worker carry things
 	my_inventory = ComponentInventory.new()
 	my_inventory.setup(5)
+	set_meta("component_inventory", my_inventory)
 	
 	#let the worker have a family including a home
 	my_family = ComponentFamily.new()
-	
-	set_meta("component_sight", my_sight_component)
-	set_meta("component_movement", my_movement_component)
-	set_meta("component_charstats", my_char_stats)
-	set_meta("component_inventory", my_inventory)
-	set_meta("component_tasks", my_tasks)
 	set_meta("component_family", my_family)
 	
 	
